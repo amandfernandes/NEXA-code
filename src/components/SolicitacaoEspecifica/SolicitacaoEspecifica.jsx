c6bc38beca763d263ca07b489967034389025316
SiabaFC <felipesiaba@gmail.com>
2024-05-08 16:21:22 -0300

    Link para botao acessar na pagina Home

--- src/components/SolicitacaoEspecifica/SolicitacaoEspecifica.jsx
+++ src/components/SolicitacaoEspecifica/SolicitacaoEspecifica.jsx
@@ -1,20 +1,21 @@
 /* Esse componente vai tratar da solicitação detalhada */
-import React from 'react';
-import { Table, Th, Td, Tr, Button, TableContainer } from './Style';
+import React from "react";
+import { Table, Th, Td, Tr, Button, TableContainer } from "./Style";
 
 const SolicitacaoEspecifica = () => {
   return (
     <div>
-        <p><label>Cliente:</label> Cliente</p>
-        <p><label>Data:</label> Data</p>
-        <p><label>Status:</label> Status</p>
+          <p>
+            <label>Cliente:</label> Cliente
+          </p>
+          <p>
+            <label>Data:</label> Data
+          </p>
+          <p>
+            <label>Status:</label> Status
+          </p>
     </div>
   );
 };
 
 export default SolicitacaoEspecifica;
-/*      id: request.id,
-client: request.client,
-date: request.date,
-status: request.status,
-forms: request.forms, */
